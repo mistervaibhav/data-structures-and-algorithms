dcb2f7146fb9467a0c76e8fb91da7c079f340fc8
mistervaibhav <vaibhav.singh@fashinza.com>
2023-01-01 11:04:13 +0530

    Q: multiply numbers with recursion

---
+++ recursion/multiply.py
@@ -0,0 +1,19 @@
+# Multiply two numbers without mathematical operators / with recursion
+from utils.main import timer
+
+@timer
+def multiply(x, y):
+    
+    if y == 0:
+        return 0
+    
+    if y > 0:
+        return x + multiply(x, y - 1)
+    
+    if y < 0:
+        return -(x + multiply(x, y - 1))
+
+
+
+if __name__ == "__main__":
+    #  print(multiply(300,413))
